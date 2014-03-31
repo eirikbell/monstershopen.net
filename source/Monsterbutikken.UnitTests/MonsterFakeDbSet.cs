a7b625ea0cbce1deb32f4f84c7896fa9a330ade7
Eirik Bell <eirik.bell@bouvet.no>
2014-03-31 10:15:01 +0200

    Added FakeDbSet

---
+++ source/Monsterbutikken.UnitTests/MonsterFakeDbSet.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using DomainModel;
+
+namespace Monsterbutikken.UnitTests
+{
+    public class MonsterFakeDbSet : FakeDbSet<Monster>
+    {
+        public override Monster Find(params object[] keyValues)
+        {
+            var id = (int)keyValues.First();
+            return List.Find(f => f.MonsterId == id);
+        }
+    }
+}
