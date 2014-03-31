90303fb9494cc9b93cebf1e69c0c96d603c6004d
Eirik Bell <eirik.bell@bouvet.no>
2014-03-31 09:19:55 +0200

    Forgot to add interface

---
+++ source/DataLayer/Interfaces/IMonsterRepository.cs
@@ -0,0 +1,9 @@
+﻿using DomainModel;
+
+namespace DataLayer.Interfaces
+{
+    public interface IMonsterRepository : IEntityRepository<Monster, int>
+    {
+        Monster FindByName(string monsterName);
+    }
+}
