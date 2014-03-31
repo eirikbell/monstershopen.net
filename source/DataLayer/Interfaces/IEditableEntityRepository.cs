1af325dea84946e939fa2951c7656530527c413f
Eirik Bell <eirik.bell@bouvet.no>
2014-03-31 08:23:34 +0200

    State and repository

---
+++ source/DataLayer/Interfaces/IEditableEntityRepository.cs
@@ -0,0 +1,13 @@
+﻿using DomainModel;
+
+namespace DataLayer.Interfaces
+{
+    public interface IEditableEntityRepository<TEntity> : IEntityRepository<TEntity>
+        where TEntity : IObjectWithState
+    {
+        void InsertOrUpdateGraph(TEntity entityGraph);
+        void InsertOrUpdate(TEntity entity);
+        void Delete(int entityId);
+        int Save();
+    }
+}
---
+++ source/DataLayer/Interfaces/IEntityRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using DomainModel;
+
+namespace DataLayer.Interfaces
+{
+    public interface IEntityRepository<TEntity> : IDisposable
+        where TEntity : IObjectWithState
+    {
+        TEntity Find(int entityId);
+        ICollection<TEntity> All { get; }
+    }
+}
